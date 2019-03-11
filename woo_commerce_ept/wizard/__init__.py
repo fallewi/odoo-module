f7d74f55599672a1ea00647366cc4eb57890b756
unknown <myducnam@gmail.com>
2019-03-11 22:46:53 +0700

    update

--- woo_commerce_ept/wizard/__init__.py
+++ woo_commerce_ept/wizard/__init__.py
@@ -1,4 +1,4 @@
-import config
-import woo_process_import_export
-import woo_cancel_order_wizard
-import installer
+from . import config
+from . import woo_process_import_export
+from . import woo_cancel_order_wizard
+from . import installer
